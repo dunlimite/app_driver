e9acf52eca581ec8416e6fc31f263a08e5e70495
DIP <dipankar@acuitysoftware.in>
2026-02-23 13:58:52 +0530

    test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,9 +6,10 @@ import 'react-native';
 import React from 'react';
 // import App from '../App';
 import Profile from '../src/pages/Profile';
+import Login from '../src/pages/Login';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<></>);
+  renderer.create(<Login />);
 });
